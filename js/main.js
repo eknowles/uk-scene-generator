c3f0b64a217d36a67c08fb5d8f419e0acf908c80
Edward Knowles <mail@eknowles.com>
2016-04-01 20:51:51 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function Article(seed, data) {
+    this.team = '';
+    this.articleType = 1; // 1 = new player to existing roster, 2 = team disbanded, 3 = new formed team
+    
+}
+
+$(function(){
+
+});
